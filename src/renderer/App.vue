f6b0cb6afbd74c7536290efa33c8a101ffad3914
dingyun <dingyun@hitb.com.cn>
2018-01-31 21:12:34 +0800

    新建页面

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -6,7 +6,7 @@
 
 <script>
   export default {
-    name: 'dy-client',
+    name: 'clinet',
   };
 </script>
 
--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -7,8 +7,33 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'landing-page',
+      name: 'home-page',
       component: require('@/components/LandingPage').default,
+    },
+    {
+      path: '/login',
+      name: 'login-page',
+      component: require('@/components/LoginPage').default,
+    },
+    {
+      path: '/edit',
+      name: 'edit-page',
+      component: require('@/components/EditPage').default,
+    },
+    {
+      path: '/stat',
+      name: 'stat-page',
+      component: require('@/components/StatPage').default,
+    },
+    {
+      path: '/library',
+      name: 'library-page',
+      component: require('@/components/LibraryPage').default,
+    },
+    {
+      path: '/system',
+      name: 'system-page',
+      component: require('@/components/SystemPage').default,
     },
     {
       path: '*',
