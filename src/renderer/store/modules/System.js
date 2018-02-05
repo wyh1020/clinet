dfd236398f81b83c08e43371fd4f0dfe006b5623
dingyun <dingyun@hitb.com.cn>
2018-02-05 22:19:50 +0800

    用readline读取CSV文件，大于5M的不读

--- src/renderer/store/modules/System.js
+++ src/renderer/store/modules/System.js
@@ -18,21 +18,27 @@ const mutations = {
       dir: 'C:\\hitbdata\\',
       base: path1
     });
-    const stat = fs.lstatSync(file);
-    if (stat.isDirectory()) {
-      console.log('目录不能导入，请选择文件')
-    } else {
-      const fRead = fs.createReadStream(file);
-      const fReadline = readline.createInterface({ input: fRead });
-      let i = 0
-      fReadline.on('line', (line) => {
-        i += 1
-        console.log(line);
-        console.log(i)
-        if (i > 10) { fReadline.close() }
-      })
-      // todo:
-    }
+    fs.lstat(file, (err, stat) => {
+      if (stat.isDirectory()) {
+        console.log('目录不能导入，请选择文件')
+      } else if (stat.size < 1000 * 5000) {
+        const fRead = fs.createReadStream(file);
+        const fReadline = readline.createInterface({ input: fRead });
+        let i = 0
+        fReadline.on('close', () => {
+          console.log('readline close...');
+        });
+        fReadline.on('line', (line) => {
+          i += 1
+          console.log(line);
+          console.log(i)
+        })
+        // fReadline.close()
+      } else {
+        console.log(stat.size)
+        console.log('文件大于5M，无法导入，请拆成小文件')
+      }
+    })
   }
 };
 
