88992b191f7365e62d5afec832d2c13caee41261
duanzhichao <duanzhichao@hitb.com.cn>
2018-03-06 14:09:41 +0800

    修复当点击列时横轴菜单bug

--- src/renderer/utils/ChartData.js
+++ src/renderer/utils/ChartData.js
@@ -1,7 +1,7 @@
 // (数据, 行集合)
 export default function chartData(table, xid = [], yid = []) {
-  const xArr = []
-  let yArr = ['org', 'time']
+  let xArr = []
+  let yArr = [0, 1]
   const stat = []
   // 按照逗号切分并取得表头
   table = table.map(x => x.split(','))
@@ -16,11 +16,19 @@ export default function chartData(table, xid = [], yid = []) {
   }
   // 去除多余的行
   if (xArr.length === 0) {
-    xArr.push(th);
+    xArr.push(th)
+  } else {
+    xArr = []
   }
+  // 删除第一行点击
+  xid = xid.filter(t => t !== 0)
   // 选中行添加
   xid.map(x => xArr.push(table[x]))
   // 按照字段去除对应结果
   xArr.map(xs => stat.push(yArr.map(x => xs[x])))
+
+  // console.log(stat);
+  // console.log(stat[1]);
+  // console.log('==============');
   return stat
 }
