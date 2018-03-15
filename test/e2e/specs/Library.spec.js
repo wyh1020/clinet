c163f373551a9af5e63de63bdd44753068efaa0e
tanyawen <tanyawen@hitb.com.cn>
2018-03-15 09:44:36 +0800

    修改stat测试代码错误问题

--- test/e2e/specs/Library.spec.js
+++ test/e2e/specs/Library.spec.js
@@ -5,7 +5,7 @@ describe('Library', function () {
   afterEach(utils.afterEach);
 
   it('Library-测试1', function () {
-    this.timeout(10000)
+    this.timeout(20000)
     // 1、点击login页面的login-button
     return this.app.client.click('#login')
     // 等待底部通知框出现'未注册用户登陆！'提示，进入Home页
@@ -47,14 +47,14 @@ describe('Library', function () {
         expect(rightpanel).to.be.an('array');
       })
     // 2.1.4、点击工具栏的编辑数据(remote-file)，进入编辑页面，编辑页面右侧显示当前数据，左侧显示第四行高亮的内容（传入id到编辑页面用于返回）
-      // .click('#library-edit')
-      // .waitUntilTextExists('#edit-input', '')
-      // .getText('#edit-input')
-      // .then(function (editText) {
-      //   expect(editText).to.equal('');
-      // })
-      // .click('#edit-last-nav')
-      // .waitUntilTextExists('#notice-bar', '翻页')
+      .click('#library-edit')
+      .waitUntilTextExists('#edit-editbar-input', '')
+      .getText('#edit-editbar-input')
+      .then(function (editText) {
+        expect(editText).to.equal('');
+      })
+      .click('#edit-leftbar-back')
+      .waitUntilTextExists('#notice-bar', 'CSV文件读取成功！')
     // 2.1.5、点击维度选择(library-drop)，显示维度下拉选项
     // 2.1.5.1、工具栏的维度选择-机构(library-org)，左侧列表显示当前数据内所有机构，提示：机构维度选择成功，若机构列无内容，提示：无机构维度！
       .click('#library-dropdown')
@@ -67,7 +67,6 @@ describe('Library', function () {
       // .click('#library-leftlist')
       // .getText('#library-rightpanel')
       // .then(function (leftlist) {
-      //   console.log(leftlist);
       //   expect(leftlist).to.be.an('array');
       // })
     // 2.1.5.1.1、点击左侧列表(library-leftlist)，右侧表中显示所选机构的对应数据，若右侧表中无数据显示，提示：未找到对应数据！
--- test/e2e/specs/Stat.spec.js
+++ test/e2e/specs/Stat.spec.js
@@ -5,7 +5,7 @@ describe('Stat', function () {
   afterEach(utils.afterEach);
 
   it('Stat-测试1', function () {
-    this.timeout(8000)
+    this.timeout(25000)
     // 1、点击login页面的login-button
     return this.app.client.click('#login')
     // 等待底部通知框出现'未注册用户登陆！'提示，进入Home页
@@ -25,70 +25,133 @@ describe('Stat', function () {
       })
     // 2.1.1.1 点击rightPanel数据表的单条或多条数据高亮显示 rightPanel的chart及提示显示当前数据的图表及提示内容
       // .click('.stat-right-table-tr')
-      // .waitUntilWindowLoaded(1000)
+      // .waitUntilWindowLoaded(8000)
       // .click('#stat-right-table-td')
       // .waitUntilWindowLoaded(1000)
       // .getText('.stat-right-table-tr')
-      // // .then(function (rightTableTr) {
-      // //   console.log(rightTableTr)
-      // // })
+      // .then(function (rightTableTr) {
+      //   // console.log(rightTableTr)
+      //   expect(rightTableTr).to.be.an('array');
+      // })
     // 2.1.1.1.1 点击工具栏的加入对比 将rightPanel数据表中选中的数据加入到对比数据中
       // .click('#stat-join-contrast')
-      // .waitUntilWindowLoaded(1000)
       // .waitUntilTextExists('#notice-bar', '加入成功')
     // 2.1.1.1.2 点击工具栏的显示对比 将对比数据显示到rightPanel的数据表中
       // .click('#stat-show-contrast')
       // .waitUntilWindowLoaded(1000)
       // .getText('#stat-right-table')
-      // // .then(function (rightTableTr) {
-      // //   console.log(rightTableTr)
-      // // })
+      // .then(function (rightTableTr) {
+      //   // console.log(rightTableTr)
+      //   expect(rightTableTr).to.be.an('string');
+      // })
     // 2.1.1.1.3 点击工具栏的保存对比
-    // 2.1.1.1.4 点击工具栏的前一页 rightPanel显示当前数据的前十条数据并且当前行高亮显示 如果为第一页则提示"当前已是第一页"
-      // .click('#stat-prev-page')
-      // .waitUntilWindowLoaded(500)
-      // .getText('#stat-right-table')
-      // .then(function (rightTable) {
-      //   // console.log(rightTable)
-      //   expect(rightTable).to.be.an('string');
-      // })
-    // 2.1.1.1.5 点击工具栏的后一页 rightPanel显示当前数据的后十条数据并且当前行高亮显示 如果为最后一页则提示"当前已是最后一页"
-      // .click('#stat-next-page')
-      // .waitUntilWindowLoaded(1000)
-      // .getText('#stat-right-table')
-      // .then(function (rightTable) {
-      //   // console.log(rightTable)
-      //   expect(rightTable).to.be.an('string');
-      // })
+    // 2.1.1.1.4 点击工具栏的后一页 rightPanel显示当前数据的后十条数据并且当前行高亮显示 如果为最后一页则提示"当前已是最后一页"
+      .click('#stat-next-page')
+      .waitUntilWindowLoaded(1000)
+      .getText('#stat-right-table')
+      .then(function (rightTable) {
+        expect(rightTable).to.be.an('string');
+      })
+    // 2.1.1.1.5 点击工具栏的前一页 rightPanel显示当前数据的前十条数据并且当前行高亮显示 如果为第一页则提示"当前已是第一页"
+      .click('#stat-prev-page')
+      .waitUntilWindowLoaded(500)
+      .getText('#stat-right-table')
+      .then(function (rightTable) {
+        expect(rightTable).to.be.an('string');
+      })
     // 2.1.1.1.6 点击工具栏的编辑数据 将当前本地文件数据显示传到Edit的rightPanel中并高亮显示当前选中数据 leftPanel显示选中的第一条数据内容
+      .click('#stat-edit-data')
+      .waitUntilTextExists('#edit-editbar-input', '')
+      .getText('#edit-editbar-input')
+      .then(function (editText) {
+        expect(editText).to.equal('');
+      })
+      .click('#edit-leftbar-back')
+      .waitUntilTextExists('#notice-bar', 'CSV文件读取成功！')
     // 2.1.1.1.7 点击工具栏的左图选择 下拉框显示可选图形
-      // .click('#stat-left-chart')
-      // // .waitUntilTextExists('#stat-left-bar', '柱状图')
-      // .hasFocus('[aria-labelledby="stat-left-chart"]')
-      // .then(function (rightTable) {
-      //   console.log(rightTable)
-      //   // expect(rightTable).to.be.an('string');
-      // })
     // 2.1.1.1.7.1 点击柱状图 rightPanel左部chart数据展示为柱状图
-      // .click('#stat-right-chart')
-      // // .waitUntilTextExists('#stat-left-discount', '柱状图')
-      // .hasFocus('[aria-labelledby="stat-left-chart"]')
-      // .then(function (rightTable) {
-      //   console.log(rightTable)
-      //   // expect(rightTable).to.be.an('string');
-      // })
+      .click('#stat-left-chart')
+      .click('#stat-left-chart-bar')
+      // .waitUntilTextExists('#stat-left-bar', '柱状图')
+      .hasFocus('[aria-labelledby="stat-left-chart"]')
+      .then(function (rightTable) {
+        // console.log(rightTable)
+        expect(rightTable).to.equal(false)
+      })
     // 2.1.1.1.7.2 点击折线图 rightPanel左部chart数据展示为折线图
+      .click('#stat-left-chart')
+      .click('#stat-left-chart-discount')
+      // .waitUntilTextExists('#stat-left-bar', '折线图')
+      .hasFocus('[aria-labelledby="stat-left-chart"]')
+      .then(function (rightTable) {
+        // console.log(rightTable)
+        expect(rightTable).to.equal(false)
+      })
     // 2.1.1.1.7.3 点击散点图 rightPanel左部chart数据展示为散点图
+      .click('#stat-left-chart')
+      .click('#stat-left-chart-scatter-plot')
+      // .waitUntilTextExists('#stat-left-bar', '散点图')
+      .hasFocus('[aria-labelledby="stat-left-chart"]')
+      .then(function (rightTable) {
+        // console.log(rightTable)
+        expect(rightTable).to.equal(false)
+      })
     // 2.1.1.1.7.4 点击雷达图 rightPanel左部chart数据展示为雷达图
+      .click('#stat-left-chart')
+      .click('#stat-left-chart-radar-map')
+      // .waitUntilTextExists('#stat-left-bar', '雷达图')
+      .hasFocus('[aria-labelledby="stat-left-chart"]')
+      .then(function (rightTable) {
+        // console.log(rightTable)
+        expect(rightTable).to.equal(false)
+      })
     // 2.1.1.1.8 点击工具栏的右图选择 下拉框显示可选图形
     // 2.1.1.1.8.1 点击柱状图 rightPanel右部chart数据展示为柱状图
+      .click('#stat-right-chart')
+      .click('#stat-right-chart-bar')
+      // .waitUntilTextExists('#stat-right-bar', '柱状图')
+      .hasFocus('[aria-labelledby="stat-right-chart"]')
+      .then(function (rightTable) {
+        // console.log(rightTable)
+        expect(rightTable).to.equal(false)
+      })
     // 2.1.1.1.8.2 点击折线图 rightPanel右部chart数据展示为折线图
+      .click('#stat-right-chart')
+      .click('#stat-right-chart-discount')
+      // .waitUntilTextExists('#stat-right-bar', '折线图')
+      .hasFocus('[aria-labelledby="stat-right-chart"]')
+      .then(function (rightTable) {
+        // console.log(rightTable)
+        expect(rightTable).to.equal(false)
+      })
     // 2.1.1.1.8.3 点击散点图 rightPanel右部chart数据展示为散点图
+      .click('#stat-right-chart')
+      .click('#stat-right-chart-scatter-plot')
+      // .waitUntilTextExists('#stat-right-bar', '散点图')
+      .hasFocus('[aria-labelledby="stat-right-chart"]')
+      .then(function (rightTable) {
+        // console.log(rightTable)
+        expect(rightTable).to.equal(false)
+      })
     // 2.1.1.1.8.4 点击雷达图 rightPanel右部chart数据展示为雷达图
+      .click('#stat-right-chart')
+      .click('#stat-right-chart-radar-map')
+      // .waitUntilTextExists('#stat-right-bar', '雷达图')
+      .hasFocus('[aria-labelledby="stat-right-chart"]')
+      .then(function (rightTable) {
+        // console.log(rightTable)
+        expect(rightTable).to.equal(false)
+      })
     // 2.1.1.1.9 点击工具栏的维度选择 下拉框显示可选维度
     // 2.1.1.1.9.1 点击机构 leftPanel显示可选机构维度
+      .click('#stat-right-dimension')
+      .click('#stat-right-dimension-org')
     // 2.1.1.1.9.2 点击时间 leftPanel显示可选时间维度
+      .click('#stat-right-dimension')
+      .click('#stat-right-dimension-time')
     // 2.1.1.1.9.3 点击病种 leftPanel显示可选病种维度
+      .click('#stat-right-dimension')
+      .click('#stat-right-dimension-disease')
     // 2.1.1.2 点击工具栏的前一页 rightPanel显示当前数据的前十条数据 如果为第一页则提示"当前已是第一页"
     // 2.1.1.3 点击工具栏的后一页 rightPanel显示当前数据的后十条数据 如果为最后一页则提示"当前已是最后一页"
     // 2.1.1.4 点击工具栏的编辑数据 将当前本地文件数据显示传到Edit的rightPanel中并高亮显示当前选中数据 leftPanel默认显示第一条数据内容
