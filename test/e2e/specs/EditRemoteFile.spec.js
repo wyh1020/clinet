c04399101d4604c86b952500f6b39c3a5bf9d096
wangyanhui <wangyanhui@hitb.com.cn>
2018-05-18 16:48:24 +0800

    修改 edit 远程文件 以及 输入框输入文字 测试代码 以及用例

--- test/e2e/specs/EditRemoteFile.spec.js
+++ test/e2e/specs/EditRemoteFile.spec.js
@@ -19,43 +19,23 @@ describe('EditRemoteFile', function () {
         expect(editText).to.equal('');
       })
     // --------------点击 远程文件按钮-------------------------------------
+    // 2.1 点击edit 远程文件按钮
       .click('#edit-rightbar-server')
       .getText('#edit-bar-prompt')
       .then(function (editText) {
         // console.log(editText)
         expect(editText).to.be.an('string');
       })
-      // .click('#navbar-system')
-      // .click('#navbar-system-server')
-      // .click('#server-remote-list')
-      // .getText('#notice-bar')
-      // .then(function (editText) {
-      //   expect(editText).to.be.an('string');
-      // })
-      // .click('#system-td-tr1')
-      // .waitUntilWindowLoaded(2000)
-      // .getText('#system-td-tr1')
-      // .then(function (editText) {
-      //   expect(editText).to.be.an('string');
-      // })
-      // .getText('#notice-bar')
-      // .then(function (editText) {
-      //   expect(editText).to.be.an('string');
-      // })
-      // .click('#navbar-edit')
-      // .waitUntilTextExists('#edit-editbar-input', '')
-      // .click('#edit-rightbar-server')
-      // .getText('#edit-bar-prompt')
-      // .then(function (editText) {
-      //   expect(editText).to.be.an('string');
-      // })
+    // 2.2 点击导航栏 系统服务下拉
       .click('#navbar-system')
+    // 2.3 点击导航栏 系统服务 下拉 远程服务器设置 按钮
       .click('#navbar-system-server')
       .getText('#notice-bar')
       .then(function (editText) {
         // console.log(editText)
         expect(editText).to.be.an('string');
       })
+    // 2.4 点击 用户设置 按钮
       .click('#server-user-setup')
       .getText('#notice-bar')
       .then(function (editText) {
@@ -64,6 +44,7 @@ describe('EditRemoteFile', function () {
       })
       // .setValue('#server-username', 'test@hitb.com.cn')
       // .setValue('#server-password', '123456')
+    // 2.5 点击登录按钮
       .click('#server-login')
       .getText('#server-login')
       .then(function (editText) {
@@ -76,27 +57,104 @@ describe('EditRemoteFile', function () {
         // console.log(editText)
         expect(editText).to.be.an('string');
       })
+    // 2.6 点击 导航栏中 edit下拉 按钮
       .click('#navbar-edit')
       .waitUntilTextExists('#edit-editbar-input', '')
+    // 2.6 点击 右侧工具栏 远程文件 按钮
       .click('#edit-rightbar-server')
+      // .getText('#edit-rightpanellocal-table')
+      // .then(function (editText) {
+      //   console.log(editText);
+      //   expect(editText).to.not.equal('');
+      // })
+      .getText('#edit-bar-prompt')
+      .then(function (editText) {
+        expect(editText).to.be.an('string');
+      })
+    // 2.7 点击 右侧 table 第一行 文件 列表
+      .click('#edit-rightpanellocal-tr0')
+      .getText('#edit-rightpanellocal-table')
+      .then(function (editText) {
+        // console.log(editText);
+        expect(editText).to.not.equal('');
+      })
       .getText('#edit-bar-prompt')
       .then(function (editText) {
         // console.log(editText);
         expect(editText).to.be.an('string');
       })
-      // .click('.edit-rightpanellocal-tr')
+      .click('#edit-rightpanellocal-tr1')
+      .getText('#edit-leftpaneltable-table')
+      .then(function (editText) {
+        console.log(editText);
+        expect(editText).to.be.an('string');
+      })
+      // // .waitUntilTextExists('#notice-bar', '文件读取成功,但文件拥有者不允许修改')
       // .getText('#edit-bar-prompt')
       // .then(function (editText) {
       //   console.log(editText);
       //   expect(editText).to.be.an('string');
       // })
-      // .click('.edit-rightpanellocal-tr')
+    // 2.3.2 点击左侧 第一行 td 删除按钮 edit-leftpaneltable-del0
+      // .click('#edit-leftpaneltable-del0')
       // .getText('#edit-bar-prompt')
       // .then(function (editText) {
-      //   console.log(editText);
+      //   // console.log(editText);
       //   expect(editText).to.be.an('string');
       // })
+    // 2.3.2.1 点击保存按钮 edit-leftbar-preservation
+      // .click('#edit-leftbar-preservation')
+      // .getText('#edit-bar-prompt')
+      // .then(function (editText) {
+      //   // console.log(editText);
+      //   expect(editText).to.be.an('string');
+      // })
+    // 2.3.3 点击左侧 第一行 td 编辑按钮 edit-leftpaneltable-edit0
+      // .click('#edit-leftpaneltable-edit0')
+      // .getText('#edit-bar-prompt')
+      // .then(function (editText) {
+      //   // console.log(editText);
+      //   expect(editText).to.be.an('string');
+      // })
+      // .click('#edit-editbar-input')
+      // .keys('\uE015')
+      // .setValue('#edit-editbar-input', '姓名 这是个名字')
+      // .getText('#edit-bar-prompt')
+      // .then(function (editText) {
+      //   expect(editText).to.be.an('string');
+      // })
+    // 2.3.3 点击缓存 edit-leftbar-cache
+      // .click('#edit-leftbar-cache')
+      // .getText('#edit-bar-prompt')
+      // .then(function (editText) {
+      //   expect(editText).to.be.an('string');
+      // })
+    // 2.3.3.1 点击右侧 本地按钮 左侧保存按钮
+      // .click('#edit-rightbar-local')
+      // .waitUntilTextExists('#edit-bar-prompt', '读取本地文件')
+      // .click('#edit-leftbar-preservation')
+      // .getText('#edit-bar-prompt')
+      // .then(function (editText) {
+      //   // console.log(editText);
+      //   expect(editText).to.be.an('string');
+      // })
+    // 2.3.4 点击左侧 第一行 td 参考按钮 edit-leftpaneltable-ref0
+      .click('#edit-leftpaneltable-ref0')
+      .getText('#edit-bar-prompt')
+      .then(function (editText) {
+        expect(editText).to.be.an('string');
+      })
+      // .getText('#edit-rightpanellocal-table')
+      // .then(function (editText) {
+      //   // console.log(editText);
+      //   expect(editText).to.not.equal('');
+      // })
+    // 2.3.5 点击左侧 第一行 td 下载 edit-leftpaneltable-dow
+      .click('#edit-leftpaneltable-dow0')
+      .getText('#edit-bar-prompt')
+      .then(function (editText) {
+        expect(editText).to.be.an('string');
+      })
   });
 });
-
 
