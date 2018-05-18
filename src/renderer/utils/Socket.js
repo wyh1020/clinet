083ece56a719a9ae7f6f55053f13003ba091c3d7
duanzhichao <duanzhichao2008@gmail.com>
2018-05-18 10:42:03 +0800

    拆分连接和加入房间

--- src/renderer/utils/Socket.js
+++ src/renderer/utils/Socket.js
@@ -6,27 +6,30 @@ export function connect(obj, data) {
   if (data[0] !== '' && data[1] !== '' && data[2] !== '') {
     socket = new Socket(`ws://${data[0]}:8000/socket`, {});
     socket.connect();
-    channel = socket.channel('room:lobby', {})
-    channel.join()
-      .receive('ok', () => {
-        obj.$store.commit('SET_NOTICE', '加入房间成功')
-      })
-      .receive('error', () => {
-        obj.$store.commit('SET_NOTICE', '加入房间失败')
-      })
-    channel.push('加入房间', { body: `${data[2]}`, username: data[2] })
-    channel.on('新消息', (payload) => {
-      obj.$store.commit('EDIT_SET_SOCKET_RECORD', { message: payload.body, type: 'message', username: payload.username });
-    })
-    channel.on('加入房间', (payload) => {
-      obj.$store.commit('EDIT_SET_SOCKET_RECORD', { message: payload.body, type: 'info', username: payload.username });
-    })
-    channel.on('离开房间', (payload) => {
-      obj.$store.commit('EDIT_SET_SOCKET_RECORD', { message: payload.body, type: 'info', username: payload.username });
-    })
   } else {
     obj.$store.commit('SET_NOTICE', '未登录用户,请在系统服务-用户设置内登录');
   }
+}
+
+export function join(obj, filename, username) {
+  channel = socket.channel(`room:${filename}`, {})
+  channel.join()
+    .receive('ok', () => {
+      obj.$store.commit('SET_NOTICE', '加入房间成功')
+    })
+    .receive('error', () => {
+      obj.$store.commit('SET_NOTICE', '加入房间失败')
+    })
+  channel.push('加入房间', { body: username, username: username })
+  channel.on('新消息', (payload) => {
+    obj.$store.commit('EDIT_SET_SOCKET_RECORD', { message: payload.body, type: 'message', username: payload.username });
+  })
+  channel.on('加入房间', (payload) => {
+    obj.$store.commit('EDIT_SET_SOCKET_RECORD', { message: payload.body, type: 'info', username: payload.username });
+  })
+  channel.on('离开房间', (payload) => {
+    obj.$store.commit('EDIT_SET_SOCKET_RECORD', { message: payload.body, type: 'info', username: payload.username });
+  })
 }
 
 export function message(obj, message, username = '') {
