838c7698401592bfeb207b497bade8c1363edc98
duanzhichao <duanzhichao2008@gmail.com>
2018-05-25 09:59:18 +0800

    修正账号被顶掉的bug

--- src/renderer/utils/Socket.js
+++ src/renderer/utils/Socket.js
@@ -9,11 +9,11 @@ let username = ''
 export function socketConnect(obj, data) {
   // if (data[0] !== '' && data[1] !== '' && data[2] !== '') {
   socket = new Socket(`ws://${data[0]}:8000/socket`, { params: { token: 'a token', username: data[2] } });
-  username = data[2]
   socket.connect();
-  channel2 = socket.channel('online:list', { username: username, password: data[3] })
+  channel2 = socket.channel('online:list', { username: data[2], password: data[3] })
   channel2.join()
     .receive('ok', () => {
+      username = data[2]
       obj.$store.commit('SET_NOTICE', '远程服务用户登录成功')
     })
     .receive('error', (err) => {
@@ -21,9 +21,11 @@ export function socketConnect(obj, data) {
     })
   channel2.push('用户信息', {})
   channel2.on('用户信息', (res) => {
-    obj.$store.commit('SYSTEM_SET_USER', ['用户登录成功', res.user])
-    obj.$store.commit('SYSTEM_SET_SERVER', ['', data[0], data[1]])
-    obj.$store.commit('SYSTEM_SET_CONNECT_INFO', true)
+    if (obj.$store.state.System.user.login === false) {
+      obj.$store.commit('SYSTEM_SET_USER', ['用户登录成功', res.user])
+      obj.$store.commit('SYSTEM_SET_SERVER', ['', data[0], data[1]])
+      obj.$store.commit('SYSTEM_SET_CONNECT_INFO', true)
+    }
   })
   channel2.on('邀请加入', (r) => {
     if (r.invite === username) {
