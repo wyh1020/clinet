659847deb680e32b98a1078accded6e5087894e5
duanzhichao <duanzhichao2008@gmail.com>
2018-06-01 08:09:03 +0800

    拆分系统设置多个用户方法

--- src/renderer/components/SystemPage/ServerBar.vue
+++ src/renderer/components/SystemPage/ServerBar.vue
@@ -13,21 +13,21 @@
           <a class="nav-link text-light" href="#"> 用户设置 <span class="sr-only">(current)</span></a>
         </li>
         <li v-if="toolbar === 'getUsers' && user.login === false">
-          <a class="nav-link text-light" href="#" v-on:click="systemUsers('login')" id="server-login">登录</a>
+          <a class="nav-link text-light" href="#" v-on:click="loginUser()" id="server-login">登录</a>
         </li>
         <li v-if="toolbar === 'getUsers' && user.login === false">
-          <a class="nav-link text-light" href="#" v-on:click="systemUsers('insert')">新建</a>
+          <a class="nav-link text-light" href="#" v-on:click="insertUserPage()">新建</a>
         </li>
         <li v-if="toolbar === 'getUsers' && user.login === true" id = "server-user-change">
-          <a class="nav-link text-light" href="#" v-on:click="systemUsers('update')">修改</a>
+          <a class="nav-link text-light" href="#" v-on:click="updateUserPage()">修改</a>
         </li>
         <li v-if="toolbar === 'createUsers'">
-          <a class="nav-link text-light" href="#" v-on:click="systemUsers('regiest')">注册</a>
+          <a class="nav-link text-light" href="#" v-on:click="insertUser()">注册</a>
         </li>
         <li v-if="toolbar === 'upUsers'" id = "server-user-ischange">
-          <a class="nav-link text-light" href="#" v-on:click="systemUsers('updataUser')">确认修改</a>
+          <a class="nav-link text-light" href="#" v-on:click="updateUser()">确认修改</a>
         </li>
-        <li class="nav-item active" v-on:click="systemUsers('docPower')" v-if="toolbar === 'getUsers' && user.login === true" id = "server-user-changepower">
+        <li class="nav-item active" v-on:click="docUser()" v-if="toolbar === 'getUsers' && user.login === true" id = "server-user-changepower">
           <a class="nav-link text-light" href="#"> 文件权限修改 <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item dropdown" v-on:click='getOrgs()' id="server-org-setup">
@@ -40,27 +40,27 @@
           </div>
         </li>
         <li class="nav-item active" v-if="toolbar === 'getOrgs' && orgPage === 'getOrg'" id = "server-user-neworg">
-          <a class="nav-link text-light" href="#" v-on:click="systemUsers('orgNew')"> 新建机构 <span class="sr-only">(current)</span></a>
+          <a class="nav-link text-light" href="#" v-on:click="insertOrgPage()"> 新建机构 <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item active" v-if="toolbar === 'createOrgs' && orgPage === 'getOrg' && orgName ==='insert'" id = "server-user-addorg">
-          <a class="nav-link text-light" href="#" v-on:click="systemUsers('orgInsert')"> 添加机构 <span class="sr-only">(current)</span></a>
+          <a class="nav-link text-light" href="#" v-on:click="insertOrg()"> 添加机构 <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item active" v-if="toolbar === 'createOrgs' && orgPage === 'getOrg' && orgName ==='update'">
-          <a class="nav-link text-light" href="#" v-on:click="systemUsers('orgUpdate')"> 机构修改 <span class="sr-only">(current)</span></a>
+          <a class="nav-link text-light" href="#" v-on:click="updateOrg()"> 机构修改 <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item active" v-if="toolbar === 'getOrgs' && orgPage === 'getDepartment'">
-          <a class="nav-link text-light" href="#" v-on:click="systemUsers('newDep')"> 新建科室 <span class="sr-only">(current)</span></a>
+          <a class="nav-link text-light" href="#" v-on:click="insertDepPage()"> 新建科室 <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item active" v-if="toolbar === 'createDepartments' && orgPage === 'getDepartment' && orgName ==='insertDep'">
-          <a class="nav-link text-light" href="#" v-on:click="systemUsers('insertDep')"> 添加科室 <span class="sr-only">(current)</span></a>
+          <a class="nav-link text-light" href="#" v-on:click="insertDep()"> 添加科室 <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item active" v-if="toolbar === 'createDepartments' && orgPage === 'getDepartment' && orgName ==='updateDep'">
-          <a class="nav-link text-light" href="#" v-on:click="systemUsers('updateDep')"> 科室修改 <span class="sr-only">(current)</span></a>
+          <a class="nav-link text-light" href="#" v-on:click="updateDep()"> 科室修改 <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item active" v-on:click='getPersons()' id="server-people-setup" v-if="this.$store.state.System.userPower === 1">
           <a class="nav-link text-light" href="#"> 人员设置 <span class="sr-only">(current)</span></a>
         </li>
-        <li class="nav-item active" v-if="this.$store.state.System.userPower === 1 && toolbar === 'getPersons' && orgPage === 'getPerson'" v-on:click="systemUsers('personUpdate')">
+        <li class="nav-item active" v-if="this.$store.state.System.userPower === 1 && toolbar === 'getPersons' && orgPage === 'getPerson'" v-on:click="updatePerson()">
           <a class="nav-link text-light" href="#"> 人员修改 <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item active" v-on:click='upHelp()'>
@@ -218,77 +218,85 @@
           }
         }
       },
-      systemUsers: function (value) {
-        if (value === 'login') {
-          const reg = /^([0-9A-Za-z\-_.]+)@([0-9a-z]+\.[a-z]{2,3}(\.[a-z]{2})?)$/g
-          const user = this.userLogin
-          if (reg.test(user.username)) {
-            this.$store.commit('SYSTEM_SET_SERVER', this.$store.state.System.file[1].split(','))
-            socketConnect(this, [this.server, this.port, user.username, user.password])
-          } else if (Array.from(user.username.split(' ')).length === 12) {
-            const key = Object.keys(global.hitbdata.blockchain)[0]
-            const server = global.hitbdata.blockchain[key][0];
-            this.$store.commit('BLOCK_SET_SERVER', server)
-            open(this, [server[0], server[1], user.username]);
-          }
-        } else if (value === 'insert') {
-          this.$store.commit('SYSTEM_SET_TOOLBAR', 'createUsers')
-        } else if (value === 'regiest') {
+      loginUser: function () {
+        const reg = /^([0-9A-Za-z\-_.]+)@([0-9a-z]+\.[a-z]{2,3}(\.[a-z]{2})?)$/g
+        const user = this.userLogin
+        if (reg.test(user.username)) {
           this.$store.commit('SYSTEM_SET_SERVER', this.$store.state.System.file[1].split(','))
-          // 邮箱,密码,年龄.电话
-          const reg = /^([0-9A-Za-z\-_.]+)@([0-9a-z]+\.[a-z]{2,3}(\.[a-z]{2})?)$/g
-          let a = 1;
-          if (reg.test(this.registerInfo.email)) {
-            a = 1
-          } else {
-            a = 0
-            this.$store.commit('SET_NOTICE', '用户名或邮箱输入错误');
-          }
-          if (a === 1) {
-            sRegister(this, [this.server, this.port, this.registerInfo])
-          }
-        } else if (value === 'update') {
-          this.$store.commit('SYSTEM_SET_TOOLBAR', 'upUsers')
-        } else if (value === 'updataUser') {
-          sUpdateUser(this, [this.server, this.port, this.$store.state.System.user.id, this.upUserInfo])
-        } else if (value === 'docPower') {
-          // sUpdateDocPower
-          sUpdateUser(this, [this.server, this.port, this.$store.state.System.user.id, { is_show: !this.$store.state.System.user.is_show }])
-        } else if (value === 'personUpdate') {
-          sUpdateUser(this, [this.$store.state.System.server, this.$store.state.System.port, this.personId, this.personUpdate])
-        } else if (value === 'orgNew') {
-          this.$store.commit('SYSTEM_SET_ORG_NAME', 'insert');
-          this.$store.commit('SYSTEM_SET_TOOLBAR', 'createOrgs')
-        } else if (value === 'orgInsert') {
-          const reg = [/^([0-9A-Za-z\-_.]+)@([0-9a-z]+\.[a-z]{2,3}(\.[a-z]{2})?)$/g, /^1[0-9]{10}$/]
-          let a = 1
-          let b = 1
-          if (reg[0].test(this.orgInfo.email)) {
-            a = 1
-          } else {
-            a = 0
-            this.$store.commit('SET_NOTICE', '邮箱输入错误');
-          }
-          if (reg[1].test(this.orgInfo.tel)) {
-            b = 1
-          } else {
-            b = 0
-            this.$store.commit('SET_NOTICE', '电话输入错误');
-          }
-          if (a * b === 1) {
-            sCreateOrg(this, [this.$store.state.System.server, this.$store.state.System.port, this.orgInfo])
-          }
-        } else if (value === 'orgUpdate') {
-          sUpdateOrg(this, [this.$store.state.System.server, this.$store.state.System.port, this.orgId, this.orgInfo])
-        } else if (value === 'newDep') {
-          this.$store.commit('SYSTEM_SET_ORG_NAME', 'insertDep');
-          this.$store.commit('SYSTEM_SET_TOOLBAR', 'createDepartments')
-        } else if (value === 'insertDep') {
-          sCreateDepart(this, [this.$store.state.System.server, this.$store.state.System.port, this.$store.state.System.user, this.departmentInfo]);
-        } else if (value === 'updateDep') {
-          sUpdateDepart(this, [this.$store.state.System.server, this.$store.state.System.port, this.depId, this.departmentInfo]);
-          console.log('修改')
+          socketConnect(this, [this.server, this.port, user.username, user.password])
+        } else if (Array.from(user.username.split(' ')).length === 12) {
+          const key = Object.keys(global.hitbdata.blockchain)[0]
+          const server = global.hitbdata.blockchain[key][0];
+          this.$store.commit('BLOCK_SET_SERVER', server)
+          open(this, [server[0], server[1], user.username]);
         }
+      },
+      insertUserPage: function () {
+        this.$store.commit('SYSTEM_SET_TOOLBAR', 'createUsers')
+      },
+      insertUser: function () {
+        this.$store.commit('SYSTEM_SET_SERVER', this.$store.state.System.file[1].split(','))
+        // 邮箱,密码,年龄.电话
+        const reg = /^([0-9A-Za-z\-_.]+)@([0-9a-z]+\.[a-z]{2,3}(\.[a-z]{2})?)$/g
+        let a = 1;
+        if (reg.test(this.registerInfo.email)) {
+          a = 1
+        } else {
+          a = 0
+          this.$store.commit('SET_NOTICE', '用户名或邮箱输入错误');
+        }
+        if (a === 1) {
+          sRegister(this, [this.server, this.port, this.registerInfo])
+        }
+      },
+      updateUserPage: function () {
+        this.$store.commit('SYSTEM_SET_TOOLBAR', 'upUsers')
+      },
+      updateUser: function () {
+        sUpdateUser(this, [this.server, this.port, this.$store.state.System.user.id, this.upUserInfo])
+      },
+      docUser: function () {
+        sUpdateUser(this, [this.server, this.port, this.$store.state.System.user.id, { is_show: !this.$store.state.System.user.is_show }])
+      },
+      updatePerson: function () {
+        sUpdateUser(this, [this.$store.state.System.server, this.$store.state.System.port, this.personId, this.personUpdate])
+      },
+      insertOrgPage: function () {
+        this.$store.commit('SYSTEM_SET_ORG_NAME', 'insert');
+        this.$store.commit('SYSTEM_SET_TOOLBAR', 'createOrgs')
+      },
+      insertOrg: function () {
+        const reg = [/^([0-9A-Za-z\-_.]+)@([0-9a-z]+\.[a-z]{2,3}(\.[a-z]{2})?)$/g, /^1[0-9]{10}$/]
+        let a = 1
+        let b = 1
+        if (reg[0].test(this.orgInfo.email)) {
+          a = 1
+        } else {
+          a = 0
+          this.$store.commit('SET_NOTICE', '邮箱输入错误');
+        }
+        if (reg[1].test(this.orgInfo.tel)) {
+          b = 1
+        } else {
+          b = 0
+          this.$store.commit('SET_NOTICE', '电话输入错误');
+        }
+        if (a * b === 1) {
+          sCreateOrg(this, [this.$store.state.System.server, this.$store.state.System.port, this.orgInfo])
+        }
+      },
+      updateOrg: function () {
+        sUpdateOrg(this, [this.$store.state.System.server, this.$store.state.System.port, this.orgId, this.orgInfo])
+      },
+      insertDepPage: function () {
+        this.$store.commit('SYSTEM_SET_ORG_NAME', 'insertDep');
+        this.$store.commit('SYSTEM_SET_TOOLBAR', 'createDepartments')
+      },
+      insertDep: function () {
+        sCreateDepart(this, [this.$store.state.System.server, this.$store.state.System.port, this.$store.state.System.user, this.departmentInfo]);
+      },
+      updateDep: function () {
+        sUpdateDepart(this, [this.$store.state.System.server, this.$store.state.System.port, this.depId, this.departmentInfo]);
       },
       upHelp: function () {
         sUpHelp(this, [this.$store.state.System.server, this.$store.state.System.port, '编辑器帮助', ''])
