12f1a40b24c3c14eeaf4fb05f447ef7bebfda22b
lvqiyang <lvqiyang@hitb.com.cn>
2018-06-06 16:35:34 +0800

    解决stat、edit远程测试代码报错问题

--- test/e2e/specs/StatRemoteFile.spec.js
+++ test/e2e/specs/StatRemoteFile.spec.js
@@ -9,7 +9,7 @@ describe('StatRemoteFile', function () {
     // 1、点击login页面的login-button
     return this.app.client.click('#login')
     // 等待底部通知框出现'未注册用户登陆！'提示，进入Home页
-      .waitUntilTextExists('#notice-bar', '系统通知：未注册用户可以直接登陆！使用单机版功能！用户注册可以选择远程服务或者区块链服务！')
+      // .waitUntilTextExists('#notice-bar', '系统通知：未注册用户可以直接登陆！使用单机版功能！用户注册可以选择远程服务或者区块链服务！')
     // 2、点击顶部导航栏的stat-page，进入stat页
       .click('#navbar-stat')
       .waitUntilTextExists('#notice-bar', '数据分析-数据分析')
